c37d87632e54fedd4d93e638d539198432de59b7
Leo Hsieh <leo0924@gmail.com>
2013-12-19 20:53:45 +0800

    hide pw

--- server/lib/api.js
+++ server/lib/api.js
@@ -1,14 +1,22 @@
 'use strict';
 
+require('sugar');
 var UserModel = require('../mongodb/model/user').UserModel;
 
 module.exports = function (app) {
   app.get('/api', function (req, res) {
-    UserModel.find(function (err, data) {
+    UserModel.find(function (err, rawData) {
       if (err) {
         console.log(err);
         res.send(500);
       }
+      var data = [];
+      rawData.forEach(function (value) {
+        data.push({
+          name: value.name,
+          email: value.email
+        });
+      });
       res.json(data);
     });
   });
