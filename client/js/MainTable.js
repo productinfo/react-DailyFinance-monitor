ec4f44038a54d6a7649c1d0777549a24d5fd1b1a
LeoAJ <leo0924@gmail.com>
2014-12-25 15:41:01 -0800

    add main table

---
+++ client/js/MainTable.js
@@ -0,0 +1,47 @@
+
+var TableRow = require('./TableRow');
+var React = require('react');
+
+
+var MainTable = React.createClass({
+  
+  // Invoked once, on both client & server before rendering occurs
+  componentWillMount: function () {
+    var self = this;
+    $.ajax({
+      method: 'GET',
+      url: '/api'
+    })
+    .done(function (data) {
+      self.setState({
+        data: data
+      });
+    })
+    .fail(function (err) {
+      console.log(err);
+    });
+  },
+
+  getInitialState: function () {
+    return {
+      data: []
+    };
+  },
+
+  render: function () {
+    return (
+      <table className="table table-bordered table-condensed">
+        <thead>
+          <tr className="success">
+            <th>Name</th>
+            <th>Email</th>
+            <th>Registered</th>
+          </tr>
+        </thead>
+        <TableRow data={this.state.data} />
+      </table>
+    );
+  }
+});
+
+module.exports = MainTable;
