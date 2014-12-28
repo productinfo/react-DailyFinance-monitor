d11740cf2703d6e76aed43a834db8d48b97d0220
LeoAJ <leo0924@gmail.com>
2014-12-27 19:25:16 -0800

    add tablerow js

---
+++ client/js/TableRow.js
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var TableRow = React.createClass({
+  render: function () {
+    return (
+      <tbody>
+        {
+          this.props.data.map(function (item) {
+            return (<tr className="active"><td>{item.name}</td><td>{item.email}</td><td><span className="glyphicon glyphicon-ok text-success"></span></td></tr>)
+          })
+        }
+      </tbody>
+    );
+  }
+});
+
+module.exports = TableRow;
